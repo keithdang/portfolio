43f298d8186881bc912dace9a7a9464a254a51a5
keith <kdang@quartermaster.house>
2021-01-20 22:14:53 -0500

    Project Template Update

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import {ProjectCardProps} from '../lib/interfaces'
 
-// interface ProjectCardProps {
-//     title: string
-//     description: string[]
-//     stack: string[]
-//     start: Date
-//     end: Date
-//     github?: string
-//     evidence?: JSX.Element | string
-//     members: number
-// }
-
 const ProjectCard:React.FC<{ obj: ProjectCardProps }> = (props) => {
   return (
       <div className="container">
         <h3>{props.obj.title}</h3>
-        <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+        <span>{props.obj.members > 1 ? `Group Project- Members: ${props.obj.members}`: 'Independent'}</span>
+        <h4>Info</h4>
+        <ul>
+            {props.obj.description.map(info=>{
+                return <li>{info}</li>
+            })}
+        </ul>
+        <h4>Languages</h4>
+        <ul>
+            {props.obj.stack.map(info=>{
+                return <li>{info}</li>
+            })}
+        </ul>
+        <a href={props.obj.github}>Go To Github</a>
       </div>
   );
 }
--- src/lib/data/Projects.tsx
+++ src/lib/data/Projects.tsx
@@ -1,13 +1,4 @@
 import * as Module from '../interfaces';
-// export const statChat = {
-//     title: 'Stat Chat',
-//     description: ['Hello', 'World'],
-//     stack: ['React','Typescript'],
-//     start: new Date(2020, 0, 5),
-//     end: new Date(2020, 0, 15),
-//     github: 'https://github.com/keithdang/stat-chat',
-//     members: 1
-// };
 
 export const hello: Module.ProjectCardProps = {
     title: 'Stat Chat',
@@ -18,3 +9,4 @@ export const hello: Module.ProjectCardProps = {
     github: 'https://github.com/keithdang/stat-chat',
     members: 1
 };
+
